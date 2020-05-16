35bb6cc82bd932df44c5e9605b9702f49ad8308d
brellin <cekakid@gmail.com>
2020-05-16 11:31:15 -0500

    full issue endpoint

--- Controllers/IssuesController.cs
+++ Controllers/IssuesController.cs
@@ -26,56 +26,50 @@ namespace CustomTrackerBackend
         // GET: api/Issues
         [Authorize]
         [HttpGet]
-        public async Task<ActionResult<Issue>> GetIssues()
+        public async Task<ActionResult<IEnumerable<Issue>>> GetIssues()
         {
             string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
             IEnumerable<Issue> userIssues = await _context.Issues.Where(i => i.UserId == userId).ToListAsync();
-            if (userIssues.Count() < 1) return NotFound("Good request, but no issues found.");
+            if (userIssues.Count() < 1) return NotFound("Good request, but no issues found");
             return Ok(userIssues);
         }
 
         // GET: api/Issue/5
+        [Authorize]
         [HttpGet("{id}")]
         public async Task<ActionResult<Issue>> GetIssue(long id)
         {
-            var issue = await _context.Issues.FindAsync(id);
+            Issue issue = await _context.Issues.FindAsync(id);
 
-            if (issue == null)
-            {
-                return NotFound();
-            }
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
-            return issue;
+            if (issue == null) return NotFound("This issue does not exist");
+
+            if (userId != issue.UserId) return Unauthorized("You are not authorized to view this issue");
+
+            return Ok(issue);
         }
 
         // PUT: api/Issue/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [Authorize]
         [HttpPut("{id}")]
         public async Task<IActionResult> PutIssue(long id, Issue issue)
         {
-            if (id != issue.Id)
-            {
-                return BadRequest();
-            }
+            if (id != issue.Id) return BadRequest("Issue ID does not match route ID");
+
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            Issue issueMatch = await _context.Issues.FindAsync(id);
+
+            if (issueMatch == null) return NotFound("This issue does not exist");
+
+            if (issueMatch.UserId != userId) return Unauthorized("You are not authorized to edit this issue");
 
             _context.Entry(issue).State = EntityState.Modified;
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!IssueExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+            await _context.SaveChangesAsync();
 
             return NoContent();
         }
@@ -83,34 +77,32 @@ namespace CustomTrackerBackend
         // POST: api/Issue
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [Authorize]
         [HttpPost]
         public async Task<ActionResult<Issue>> PostIssue(Issue issue)
         {
             _context.Issues.Add(issue);
             await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetIssue", new { id = issue.Id }, issue);
+            return CreatedAtAction("New Issue", new { id = issue.Id }, issue);
         }
 
         // DELETE: api/Issue/5
+        [Authorize]
         [HttpDelete("{id}")]
         public async Task<ActionResult<Issue>> DeleteIssue(long id)
         {
-            var issue = await _context.Issues.FindAsync(id);
-            if (issue == null)
-            {
-                return NotFound();
-            }
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            Issue issue = await _context.Issues.FindAsync(id);
+
+            if (issue == null) return NotFound("This issue does not exist");
+
+            if (userId != issue.UserId) return Unauthorized("You are not authorized to delete this issue");
 
             _context.Issues.Remove(issue);
             await _context.SaveChangesAsync();
 
             return issue;
-        }
-
-        private bool IssueExists(long id)
-        {
-            return _context.Issues.Any(e => e.Id == id);
         }
     }
 }
