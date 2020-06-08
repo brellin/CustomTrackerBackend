130b8828a006f5a3c854be18383ff5829a876b6b
brellin <cekakid@gmail.com>
2020-06-08 13:51:23 -0500

    groups error handling

--- Controllers/GroupsController.cs
+++ Controllers/GroupsController.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
@@ -23,26 +22,38 @@ namespace CustomTrackerBackend.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Group>>> GetGroups()
         {
-            string userId = User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
-            List<Group> ownedGroups = await context.Groups
-                .Include(g => g.Issues)
-                .Include(g => g.Owner)
-                .Where(g => g.OwnerId == userId)
-                .ToListAsync();
-            if (ownedGroups.Count() == 0) return NotFound("No groups found");
-            return Ok(ownedGroups);
+            try
+            {
+                string userId = User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
+                List<Group> ownedGroups = await context.Groups
+                    .Include(g => g.Issues)
+                    .Include(g => g.Owner)
+                    .Where(g => g.OwnerId == userId)
+                    .ToListAsync();
+                if (ownedGroups.Count() == 0) return NotFound("No groups found");
+                return Ok(ownedGroups);
+            }
+            catch (Exception err) { return StatusCode(500, err); }
         }
 
         // GET groups/5
         [HttpGet("{id}")]
         public async Task<ActionResult<Group>> GetGroupById(int id)
         {
-            string userId = User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
-            Group GroupMatch = context.Groups.Include(g => g.Issues).First(g => g.Id == id);
-            await Task.Delay(500);
-            if (GroupMatch == null) return NotFound();
-            if (GroupMatch.OwnerId != userId) return Unauthorized();
-            return Ok(GroupMatch);
+            try
+            {
+                string userId = User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
+                Group GroupMatch = context.Groups.Include(g => g.Issues).First(g => g.Id == id);
+                await Task.Delay(500);
+                if (GroupMatch == null) return NotFound();
+                if (GroupMatch.OwnerId != userId) return Unauthorized();
+                return Ok(GroupMatch);
+            }
+            catch (Exception err)
+            {
+                if (err.Source == "System.Linq") return NotFound($"Group with id of '{id}' does not exist");
+                return StatusCode(500, err);
+            }
         }
 
         // POST groups
@@ -50,11 +61,19 @@ namespace CustomTrackerBackend.Controllers
         public async Task<ActionResult<Group>> PostGroup(Group group)
         {
             if (!ModelState.IsValid) return UnprocessableEntity("Must include name");
-            string userId = User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
-            group.OwnerId = userId;
-            await context.AddAsync(group);
-            await context.SaveChangesAsync();
-            return Ok(context.Groups.Include(g => g.Owner).Include(g => g.Issues).First(g => g.Name == group.Name));
+            try
+            {
+                string userId = User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
+                group.OwnerId = userId;
+                await context.AddAsync(group);
+                await context.SaveChangesAsync();
+                return Ok(context.Groups.Include(g => g.Owner).Include(g => g.Issues).First(g => g.Name == group.Name));
+            }
+            catch (Exception err)
+            {
+                if (err.Source == "Microsoft.EntityFrameworkCore.Relational") return Conflict($"Group already exists with name '{group.Name}'");
+                return StatusCode(500, err);
+            }
         }
 
         // PUT groups/5
@@ -62,25 +81,41 @@ namespace CustomTrackerBackend.Controllers
         public async Task<ActionResult<Group>> PutGroup(int id, Group group)
         {
             if (!ModelState.IsValid) return UnprocessableEntity(ModelState);
-            string userId = User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
-            Group match = await context.Groups.FirstAsync(g => g.Id == id);
-            if (userId != match.OwnerId) return Unauthorized();
-            match.Name = group.Name;
-            match.OwnerId = group.OwnerId;
-            await context.SaveChangesAsync();
-            return Ok(match);
+            try
+            {
+                string userId = User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
+                Group match = await context.Groups.FirstAsync(g => g.Id == id);
+                if (userId != match.OwnerId) return Unauthorized();
+                match.Name = group.Name;
+                match.OwnerId = group.OwnerId;
+                await context.SaveChangesAsync();
+                return Ok(match);
+            }
+            catch (Exception err)
+            {
+                if (err.Source == "System.Linq") return NotFound($"Group with id of '{id}' does not exist");
+                return StatusCode(500, err);
+            }
         }
 
         // DELETE groups/5
         [HttpDelete("{id}")]
         public async Task<ActionResult> DeleteGroupById(int id)
         {
-            string userId = User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
-            Group match = await context.Groups.FirstAsync(g => g.Id == id);
-            if (userId != match.OwnerId) return Unauthorized();
-            context.Groups.Remove(match);
-            await context.SaveChangesAsync();
-            return Accepted();
+            try
+            {
+                string userId = User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
+                Group match = await context.Groups.FirstAsync(g => g.Id == id);
+                if (userId != match.OwnerId) return Unauthorized();
+                context.Groups.Remove(match);
+                await context.SaveChangesAsync();
+                return Accepted();
+            }
+            catch (Exception err)
+            {
+                if (err.Source == "System.Linq") return NotFound($"Group with id of '{id}' does not exist");
+                return StatusCode(500, err);
+            }
         }
     }
 }
